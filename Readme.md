97f94d1487dc9fbd272e1592dd1388eb695b9f02
Igor Jovic <igor.jovic@homegate.ch>
2019-05-31 13:40:51 +0200

    simplify rules

---
+++ Readme.md
@@ -0,0 +1,2 @@
+1. Run main.py - trains a model and extracts raw rule json into data/extracted_rules.jsonj
+2. Run simplify_rules.py - loads data/extracted_rules.json and eliminates redundant rules. `Eg: greater than 2 and greater than 3 becomes greater than 3` 
---
+++ expertsystems/simplify_rules.py
@@ -0,0 +1,39 @@
+import json
+
+
+def load_rules():
+    with open('../data/extracted_rules.json') as f:
+        data = json.load(f)
+        return data
+
+
+def simplify_rule(rule):
+    conditions = rule['conditions']
+    features = {condition['feature'] for condition in rule['conditions']}
+    new_conditions = []
+
+    for feature in features:
+        feature_conditions = [condition for condition in conditions if condition['feature'] == feature]
+        less_or_equal_conditions = [condition for condition in feature_conditions if condition['operator'] == 'less_or_equal']
+        greater_conditions = [condition for condition in feature_conditions if condition['operator'] == 'greater']
+        less_or_equal_conditions = sorted(less_or_equal_conditions, key=lambda k: k['value'])
+        greater_conditions = sorted(greater_conditions, key=lambda k: k['value'], reverse=True)
+
+        if len(less_or_equal_conditions) != 0:
+            new_conditions.append(less_or_equal_conditions[0])
+        if len(greater_conditions) != 0:
+            new_conditions.append(greater_conditions[0])
+
+    return {
+        'conditions': new_conditions,
+        'output': rule['output']
+    }
+
+
+if __name__ == '__main__':
+    rules = load_rules()
+
+    simplified_rules = [simplify_rule(rule=rule) for rule in rules]
+
+    with open('../data/simplified_rules.json', 'w') as outfile:
+        json.dump(simplified_rules, outfile)
