194615ac7dd73334499a95e1fb81b953cf1cf55d
Igor Jovic <igor.jovic@homegate.ch>
2019-06-11 15:18:56 +0200

    rule extraction

--- expertsystems/human_readable_rules.py
+++ expertsystems/human_readable_rules.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def load_rules():
-    with open('../data/simplified_rules.json') as f:
+    with open('../data/refactored_rules.json') as f:
         data = json.load(f)
         return data
 
@@ -14,15 +14,35 @@ operator_to_string = {
 }
 
 if __name__ == '__main__':
-    rules = load_rules()
+    config = load_rules()
+    rules = config['rules']
+    subrules = config['subrules']
     output_variable_name = 'tax'
 
     human_readable_rules = []
+    for subrule in subrules:
+        human_readable_rule = subrule['name'] + ': '
+
+        for condition in subrule['conditions']:
+            if condition['operator'] == 'between':
+                s = f'IF {condition["feature"].strip()} IS IN RANGE ({round(condition["lower_value"], 4)}, {round(condition["higher_value"], 4)}]'
+            else:
+                s = f'IF {condition["feature"].strip()} {operator_to_string[condition["operator"]]} {round(condition["value"], 4)}'
+            if human_readable_rule == f'{subrule["name"]}: ':
+                human_readable_rule = human_readable_rule + s
+            else:
+                human_readable_rule = human_readable_rule + f' AND {s}'
+
+        human_readable_rule = human_readable_rule + f' THEN {subrule["name"]}'
+        human_readable_rules.append(human_readable_rule)
+
     for rule in rules:
         human_readable_rule = ''
 
         for condition in rule['conditions']:
-            if condition['operator'] == 'between':
+            if 'subrule' in condition:
+                s = f'IF {condition["subrule"]}'
+            elif condition['operator'] == 'between':
                 s = f'IF {condition["feature"].strip()} IS IN RANGE ({round(condition["lower_value"], 4)}, {round(condition["higher_value"], 4)}]'
             else:
                 s = f'IF {condition["feature"].strip()} {operator_to_string[condition["operator"]]} {round(condition["value"], 4)}'
